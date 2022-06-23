0ec382bf5dcfbe89c332bd598a53ead75cd70a92
RaniVaradharaj <ranidvhsr31@gmail.com>
2022-06-23 16:59:14 +0530

    cab

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 
 const cors = require("cors");
 
-app.listen(process.env.PORT || 3003);
-console.log("web server started");
+app.listen(process.env.PORT || 3003, () => {
+  console.log("web server started");
+});
 
 app.use(cors({ origin: "http://localhost/3003" }));
