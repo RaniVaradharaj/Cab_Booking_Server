d7a591de3bfa760ae62c5cc4d458b1dc2af843b2
RaniVaradharaj <ranidvhsr31@gmail.com>
2022-06-24 18:02:49 +0530

    cab

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const app = express();
 
+const cors = require("cors");
+const { param } = require("express/lib/request");
+
+const mongodb = require("mongodb");
+
+const mongoClient = mongodb.MongoClient;
+const URL = "mongodb://localhost:27017";
+
 app.use(express.json());
 
-const cors = require("cors");
-
-app.listen(process.env.PORT || 3003, () => {
+app.listen(process.env.PORT || 3001, () => {
   console.log("Web server Fired");
 });
 
-app.use(cors({ origin: "http://localhost/3003" }));
+app.use(cors({ origin: "http://localhost/3000" }));
 // https://git.heroku.com/cabbookingserver.git
