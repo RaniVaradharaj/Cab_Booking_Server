8fd4f243fb97145447536ad198dadba07b23c887
RaniVaradharaj <ranidvhsr31@gmail.com>
2022-06-24 15:08:09 +0530

    commit

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 const cors = require("cors");
 
 app.listen(process.env.PORT || 3003, () => {
-  console.log("web server started");
+  console.log("Web server Fired");
 });
 
 app.use(cors({ origin: "http://localhost/3003" }));
+// https://git.heroku.com/cabbookingserver.git
